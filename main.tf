510292131abbe4eb9eacd794c2763bae2e6f1a5c
srinidhi007 <chintham.srinidhi@gmail.com>
2023-06-06 18:53:41 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,22 +7,29 @@ provider "azurerm" {
 }
 terraform {
   backend "azurerm" {
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
-    access_key           = ""
+    storage_account_name = "sristorageaccountqa"
+    container_name       = "container-qa"
+    key                  = "terraform.tfstate"
+    access_key           = "mDiQNTYfPkgCtnE3ZWV5TvFDmy1hUNyPISyWz9MyM5fylZsoTqMnPs3cfFO1b5ZI1CHkJAY5AtL7+AStZwDnkA=="
   }
 }
 module "resource_group" {
-  source               = "../../modules/resource_group"
+  source               = "./modules/resource_group"
   resource_group       = "${var.resource_group}"
   location             = "${var.location}"
 }
 module "app_service" {
-  source               = "../../modules/appservice"
-  resource_group       = "${module.resource_group.resource_group}"
+  source               = "./modules/appservice"
+  resource_group       = "${var.resource_group}"
   location             = "${var.location}"
   resource_type        = "AppService"
   application_type     = "${var.application_type}"
-  tags                 = "${loval.tags}"
+  tags                 = local.tags
+}
+
+locals {
+  tags = {
+    tier = "Test"
+    deployment = "Terraform"
+  }
 }
